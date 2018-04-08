b04fafb3e08f95134289493ce9127c624913518d
Ivan <vasilek-1997@tut.by>
2018-04-08 12:15:12 +0300

    Add phone application

---
+++ Phone/SmartMirror/SmartMirror/Controllers/BluetoothController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+
+namespace SmartMirror.Controllers
+{
+    [BroadcastReceiver(Enabled = true, Exported = true)]
+    [IntentFilter(new []
+    {
+        BluetoothAdapter.ActionStateChanged,
+        BluetoothAdapter.ActionDiscoveryStarted,
+        BluetoothAdapter.ActionDiscoveryFinished,
+        BluetoothDevice.ActionFound
+    })]
+    public class BluetoothController: BroadcastReceiver
+    {
+        private static readonly BluetoothAdapter BluetoothAdapter = BluetoothAdapter.DefaultAdapter;
+        private static readonly List<BluetoothDevice> BluetoothDevices = new List<BluetoothDevice>();
+
+        public delegate void UpdateListDevices(List<string> devices);
+        public static event UpdateListDevices DeviceDiscovered;
+
+        public delegate void DiscoverEvent();
+        public static event DiscoverEvent DiscoveryStarted;
+        public static event DiscoverEvent DiscoveryFinished;
+
+        public delegate void StateEvent(bool isEnabled);
+        public static event StateEvent StateChanged;
+
+        public static bool IsEnabled => BluetoothAdapter.IsEnabled;
+
+        public static void Enable()
+        {
+            BluetoothAdapter.Enable();
+        }
+
+        public static void StartDiscovery()
+        {
+            BluetoothDevices.Clear();
+            if (BluetoothAdapter.IsDiscovering)
+            {
+                BluetoothAdapter.CancelDiscovery();
+            }
+            BluetoothAdapter.StartDiscovery();
+        }
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            switch (intent.Action)
+            {
+                case BluetoothDevice.ActionFound:
+                    OnBluetoothDeviceActionFound(intent);
+                    break;
+                case BluetoothAdapter.ActionDiscoveryStarted:
+                    OnBluetoothAdapterActionDiscoveryStarted();
+                    break;
+                case BluetoothAdapter.ActionDiscoveryFinished:
+                    OnBluetoothAdapterActionDiscoveryFinished();
+                    break;
+                case BluetoothAdapter.ActionStateChanged:
+                    OnActionStateChanged(intent);
+                    break;
+            }
+        }
+
+        private static void OnBluetoothDeviceActionFound(Intent intent)
+        {
+            var bluetoothDevice = (BluetoothDevice) intent.GetParcelableExtra(BluetoothDevice.ExtraDevice);
+            if (!BluetoothDevices.Any(x => x.Address.Equals(bluetoothDevice.Address)))
+            {
+                BluetoothDevices.Add(bluetoothDevice);
+                DeviceDiscovered?.Invoke(BluetoothDevices.Select(x => x.Name ?? x.Address).ToList());
+            }
+        }
+
+        private static void OnBluetoothAdapterActionDiscoveryStarted()
+        {
+            DiscoveryStarted?.Invoke();
+        }
+
+        private static void OnBluetoothAdapterActionDiscoveryFinished()
+        {
+            DiscoveryFinished?.Invoke();
+        }
+
+        private static void OnActionStateChanged(Intent intent)
+        {
+            var state = intent.GetIntExtra(BluetoothAdapter.ExtraState, BluetoothAdapter.Error);
+            StateChanged?.Invoke(state.Equals((int)State.On));
+        }
+    }
+}
---
+++ Phone/SmartMirror/SmartMirror/MainActivity.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views.Animations;
+using SmartMirror.Controllers;
+
+namespace SmartMirror
+{
+    [Activity(Label = "Smart Mirror", MainLauncher = true, Icon = "@drawable/icon", 
+        Theme = "@android:style/Theme.Material.Light.NoActionBar")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            SetContentView(Resource.Layout.Main);
+
+            InitializeGui();
+            InitializeBluetoothController();
+        }
+        
+        private void InitializeGui()
+        {
+            var refreshButton = FindViewById<ImageButton>(Resource.Id.mainRefreshButton);
+            refreshButton.Click += RefreshButtonClick;
+        }
+
+        private void InitializeBluetoothController()
+        {
+            BluetoothController.DeviceDiscovered += RefreshListDevices;
+            BluetoothController.DiscoveryStarted += RefreshButtonDisable;
+            BluetoothController.DiscoveryFinished += RefreshButtonEnable;
+            BluetoothController.StateChanged += BluetoothStateChanged;
+            StartDiscoveryDevices();
+        }
+
+        private void RefreshButtonClick(object sender, EventArgs e)
+        {
+            ((ImageButton) sender).StartAnimation(AnimationUtils.LoadAnimation(this,
+                Resource.Animation.refreshButtonTapped));
+            StartDiscoveryDevices();
+        }
+
+        private void RefreshButtonEnable()
+        {
+            var refreshButton = FindViewById<ImageButton>(Resource.Id.mainRefreshButton);
+            refreshButton.Enabled = true;
+            refreshButton.ClearAnimation();
+        }
+
+        private void RefreshButtonDisable()
+        {
+            var refreshButton = FindViewById<ImageButton>(Resource.Id.mainRefreshButton);
+            refreshButton.Enabled = false;
+            void Handler(object sender, Animation.AnimationEndEventArgs args)
+            {
+                refreshButton.StartAnimation(AnimationUtils.LoadAnimation(this,
+                    Resource.Animation.refreshButtonRotate));
+            }
+            if (refreshButton.Animation == null)
+            {
+                Handler(null, null);
+            }
+            else
+            {
+               refreshButton.Animation.AnimationEnd += Handler; 
+            }
+        }
+
+        private void RefreshListDevices(List<string> devices)
+        {
+            FindViewById<ListView>(Resource.Id.mainDevicesList).Adapter =
+                new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, devices);
+        }
+
+        private void BluetoothStateChanged(bool isEnable)
+        {
+            if (isEnable)
+            {
+                StartDiscoveryDevices();
+            }
+        }
+
+        private void StartDiscoveryDevices()
+        {
+            if (BluetoothController.IsEnabled)
+            {
+                BluetoothController.StartDiscovery();
+            }
+            else
+            {
+                BluetoothController.Enable();
+            }
+        }
+    }
+}
+
